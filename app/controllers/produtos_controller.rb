4307fa348caa3ff88b407a5ae65e57d98e3e8b7d
victor cunha <vcunha1503@gmail.com>
2018-03-21 23:53:50 +0000

    Busca e validações.fixme

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -6,14 +6,23 @@ class ProdutosController < ApplicationController
     
     def create
         valores = params.require(:produto).permit :nome,:descricao,:preco,:quantidade
-        produto = Produto.create valores
-        redirect_to root_url
+        @produto = Produto.new valores
+        if @produto.save
+            redirect_to root_url
+        else
+            render :new
+        end
     end
     
     def destroy
         id = params[:id]
         Produto.destroy id
         redirect_to root_url
+    end
+    
+    def busca
+        @nome_a_buscar = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
     end
     
     def edit
