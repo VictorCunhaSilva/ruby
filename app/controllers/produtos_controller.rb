742e9701fdafca53158b151ebcab6b14d05a07e0
victor cunha <vcunha1503@gmail.com>
2018-03-21 22:41:15 +0000

    Incluir e remover

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,32 @@
 class ProdutosController < ApplicationController
     def index
-        @produtos_nome = Produto.order(:nome).limit 5
-        @produtos_preco = Produto.order(:preco).limit 2
+        @produtos_nome = Produto.order(:nome).limit 20
+        @produtos_preco = Produto.order(:preco).limit 3
+    end
+    
+    def create
+        valores = params.require(:produto).permit :nome,:descricao,:preco,:quantidade
+        produto = Produto.create valores
+        redirect_to root_url
+    end
+    
+    def destroy
+        id = params[:id]
+        Produto.destroy id
+        redirect_to root_url
+    end
+    
+    def edit
+        id = params[:id]
+        @produto = Produto.find(id)
+        render :new
+    end
+    
+    def update
+        id = params[:id]
+        @produto = Produto.find(id)
+        valores = params.require(:produto).permit :nome,:descricao,:preco,:quantidade
+        @produto.update valores
+        redirect_to root_url
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
     root "produtos#index"
+    resources :produtos, only: [:new, :create, :destroy, :edit, :update]
+    get "get/produtos/busca" => "produtos#busca", as: :busca_produto
 end
